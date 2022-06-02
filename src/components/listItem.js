32f6646920f4832b504a11b372fc936c828f8f31
zabzuki <zabzuki@protonmail.com>
2022-06-02 16:25:47 +0300

    feat: #1 finish toggle for expanding details

--- src/components/listItem.js
+++ src/components/listItem.js
@@ -4,11 +4,19 @@ liTemplate.innerHTML = `
     @import url('./src/components/listItem.css')
   </style>
   <div class="list-item">
+    <div class="li-main">
       <img class="li-img"></img>
       <span class="li-title"></span>
-    <button class="li-btn">
-      <span class="btn-text">...</span>
-    </button>
+      <button class="li-btn">
+        <span class="btn-text">...</span>
+      </button>
+    </div>
+    <div class="content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation 
+      ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
   </div>
 `;
 
@@ -17,10 +25,15 @@ export class ListItem extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(liTemplate.content.cloneNode(true));
+    this.show = false;
 
     this.listItem = this.shadowRoot.querySelector(".list-item");
     this.listImg = this.listItem.querySelector(".li-img");
     this.listTitle = this.listItem.querySelector(".li-title");
+    this.listBtn = this.listItem.querySelector(".li-btn");
+    this.toggleContent = this.listItem.querySelector(".content");
+
+    this.listBtn.addEventListener("click", () => this.toggle());
   }
 
   set movie(movieObj) {
@@ -30,4 +43,11 @@ export class ListItem extends HTMLElement {
 
     this.setAttribute("id", movieObj.id);
   }
+
+  toggle = () => {
+    this.show = !this.show;
+    console.log(this.show);
+    this.toggleContent.style.display = this.show ? "block" : "none";
+    this.dispatchEvent(new CustomEvent("showChange", { detail: this.show }));
+  };
 }
